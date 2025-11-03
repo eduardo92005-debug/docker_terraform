16b60436f23cb2a02965125f219a1855a3f0861d
eduardo92005-debug <eduardo92005@gmail.com>
2025-11-03 11:18:52 -0300

    fix: corrige nome dos resources do nginx

--- terraform/modules/nginx/healthcheck.tf
+++ terraform/modules/nginx/healthcheck.tf
@@ -1,6 +1,6 @@
-data "docker_logs" "nginx" {
-  depends_on      = [docker_container.nginx]
-  name            = docker_container.nginx.name
+data "docker_logs" "proxy" {
+  depends_on      = [docker_container.proxy]
+  name            = docker_container.proxy.name
   show_stdout     = true
   show_stderr     = true
   discard_headers = true
@@ -8,21 +8,21 @@ data "docker_logs" "nginx" {
   details = true
 }
 
-resource "time_sleep" "wait_for_nginx_logs" {
-  depends_on      = [docker_container.nginx]
+resource "time_sleep" "wait_for_proxy_logs" {
+  depends_on      = [docker_container.proxy]
   create_duration = "10s"
 }
 
-resource "null_resource" "verify_nginx_log" {
-  depends_on = [data.docker_logs.nginx, time_sleep.wait_for_nginx_logs]
+resource "null_resource" "verify_proxy_log" {
+  depends_on = [data.docker_logs.proxy, time_sleep.wait_for_proxy_logs]
 
   lifecycle {
     postcondition {
       condition = (
-        length(data.docker_logs.nginx.logs_list_string) == 0 ||
-        !can(regex("(?i)error", join("\n", data.docker_logs.nginx.logs_list_string)))
+        length(data.docker_logs.proxy.logs_list_string) == 0 ||
+        !can(regex("(?i)error", join("\n", data.docker_logs.proxy.logs_list_string)))
       )
-      error_message = "nginx container apresentou erros nos logs."
+      error_message = "Proxy Nginx container apresentou erros nos logs."
     }
   }
 }
